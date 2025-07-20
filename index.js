1b34efdf4672e47fd5083e171f206c0f9259208f
Amitesh Singh <amitesh.jy.786@gmail.com>
2025-07-20 09:21:14 +0200

    timer added for toast

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ function showToast(message, type = 'primary') {
     toastBody.textContent = message;
     toastEl.className = `toast align-items-center text-bg-${type} border-0`;
 
-    const toast = new bootstrap.Toast(toastEl);
+    const toast = new bootstrap.Toast(toastEl, {
+        delay: 5000
+    });
     toast.show();
 }
 
